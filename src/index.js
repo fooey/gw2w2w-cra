77292c05d1e22da1f916059e5c8c8e796cc9946d
Jason Rushton <github@schtuph.com>
2017-05-07 21:07:15 -0600

    enable Apollo batching

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ApolloClient, createNetworkInterface, ApolloProvider } from 'react-apollo';
+import { ApolloClient, createBatchingNetworkInterface , ApolloProvider } from 'react-apollo';
 
-const networkInterface = createNetworkInterface({ uri: 'http://localhost:4000/graphql' });
+const networkInterface = createBatchingNetworkInterface ({
+    uri: 'http://localhost:4000/graphql',
+    batchInterval: 1,
+});
 const client = new ApolloClient({ networkInterface });
 
 
