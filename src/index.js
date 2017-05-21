d3073deccd3e615d9bd4363e098dab9bd15d73be
Jason Rushton <github@schtuph.com>
2017-05-20 19:06:42 -0600

    change graphql endpoint to production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ApolloClient, createBatchingNetworkInterface , ApolloProvider } from 'react-apollo';
 
 const networkInterface = createBatchingNetworkInterface ({
-	uri: 'http://localhost:4000/graphql',
+	uri: 'https://graphql.gw2w2w.com/graphql',
 	batchInterval: 1,
 });
 const client = new ApolloClient({ networkInterface });
