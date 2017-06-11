a060e8244ba26430598cbb2df32ac27f7e13ef2b
Jason Rushton <github@schtuph.com>
2017-06-11 00:41:06 -0600

    add the rest of the timestamps

--- src/gql/match.js
+++ src/gql/match.js
@@ -4,6 +4,9 @@ export default gql`
 	query match($worldSlug: String) {
 		match(world_slug: $worldSlug) {
 			id
+			last_flipped
+			claimed_at
+			last_modified
 			region
 			world_ids
 			worlds { red_id green_id blue_id }
@@ -13,6 +16,9 @@ export default gql`
 			deaths { red green blue }
 			maps {
 				id
+				last_flipped
+				claimed_at
+				last_modified
 				type
 				scores { red green blue }
 				kills { red green blue }
@@ -22,6 +28,8 @@ export default gql`
 					type
 					owner
 					last_flipped
+					claimed_at
+					last_modified
 					claimed_by
 					points_tick
 					points_capture
